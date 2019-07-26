d7624a9322f98211f46e934ac70f87483dc6a070
unknown <sapphon@gmail.com>
2019-07-26 17:05:44 -0400

    Strings in NBT tags must be quote-wrapped now; fix Weaponized Expectorant in this way

--- src/main/resources/assets/techmage/scripts/techmage/arachnite/weaponized_expectorant.py
+++ src/main/resources/assets/techmage/scripts/techmage/arachnite/weaponized_expectorant.py
@@ -2,6 +2,6 @@
 #texture<=>dye_powder_green
 #cooldown<=>8888
 def weaponized_expectorant(): #AKA acid spit
-	spawnitem(myX(),myY(),myZ(),GOLDEN_SWORD,nbtData='{display:{Name:Ossified Secretion},ench:[{id:18,lvl:4},{id:16,lvl:1},{id:20,lvl:1}]}')
+	spawnitem(myX(),myY(),myZ(),GOLDEN_SWORD,nbtData='{display:{Name:"Ossified Secretion"},ench:[{id:18,lvl:4},{id:16,lvl:1},{id:20,lvl:1}]}')
 	yell('HORK...')
 weaponized_expectorant()
