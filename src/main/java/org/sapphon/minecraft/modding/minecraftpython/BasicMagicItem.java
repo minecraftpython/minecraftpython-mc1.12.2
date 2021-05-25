1659c39254917002766253fff5cd01554bdddfdc
Sapphon <sapphon@gmail.com>
2021-05-24 22:01:09 -0400

    Spells now check the player can pay their XP casting cost [https://trello.com/c/zSwOgyP5]

--- src/main/java/org/sapphon/minecraft/modding/minecraftpython/BasicMagicItem.java
+++ src/main/java/org/sapphon/minecraft/modding/minecraftpython/BasicMagicItem.java
@@ -10,7 +10,7 @@ import org.sapphon.minecraft.modding.minecraftpython.async.SpellCastingRunnable;
 import org.sapphon.minecraft.modding.minecraftpython.async.ThreadFactory;
 import org.sapphon.minecraft.modding.minecraftpython.interpreter.SpellInterpreter;
 import org.sapphon.minecraft.modding.minecraftpython.io.file.MinecraftPythonScriptLoader;
-import org.sapphon.minecraft.modding.minecraftpython.spells.*;
+import org.sapphon.minecraft.modding.minecraftpython.spells.ISpell;
 import org.sapphon.minecraft.modding.minecraftpython.spells.metadata.SpellMetadataConstants;
 
 public class BasicMagicItem {
@@ -21,58 +21,66 @@ public class BasicMagicItem {
         storedSpell = boundSpell;
     }
 
-	public static boolean isMagicWand(ItemStack itemStack){
+    public static boolean isMagicWand(ItemStack itemStack) {
         NBTTagCompound tagCompound = itemStack.getTagCompound();
         return (tagCompound != null && tagCompound.hasKey(SpellMetadataConstants.KEY_SPELL_PYTHON));
     }
 
-	public void recordOntoItem(ItemStack toWandify) {
-		setWandName(toWandify);
-		setWandCooldown(toWandify);
-		setWandRequiredExperience(toWandify);
-		setWandAuthor(toWandify);
-		setWandPython(toWandify);
+    public void recordOntoItem(ItemStack toWandify) {
+        setWandName(toWandify);
+        setWandCooldown(toWandify);
+        setWandRequiredExperience(toWandify);
+        setWandAuthor(toWandify);
+        setWandPython(toWandify);
     }
 
-	private void setWandAuthor(ItemStack toAuthorify) {
-		toAuthorify.setTagInfo(SpellMetadataConstants.KEY_AUTHOR_NAME, new NBTTagString(getStoredSpell().getAuthorName()));
-	}
+    private void setWandAuthor(ItemStack toAuthorify) {
+        toAuthorify.setTagInfo(SpellMetadataConstants.KEY_AUTHOR_NAME, new NBTTagString(getStoredSpell().getAuthorName()));
+    }
 
-	private void setWandRequiredExperience(ItemStack toCostify) {
-		toCostify.setTagInfo(SpellMetadataConstants.KEY_REQUIRED_EXPERIENCE_LEVEL, new NBTTagInt(getStoredSpell().getRequiredExperienceLevels()));
-	}
+    private void setWandRequiredExperience(ItemStack toCostify) {
+        toCostify.setTagInfo(SpellMetadataConstants.KEY_REQUIRED_EXPERIENCE_LEVEL, new NBTTagInt(getStoredSpell().getRequiredExperienceLevels()));
+    }
 
-	private void setWandCooldown(ItemStack toCooldownify) {
-		toCooldownify.setTagInfo(SpellMetadataConstants.KEY_COOLDOWN_MILLIS, new NBTTagLong(getStoredSpell().getCooldownInMilliseconds()));
-	}
+    private void setWandCooldown(ItemStack toCooldownify) {
+        toCooldownify.setTagInfo(SpellMetadataConstants.KEY_COOLDOWN_MILLIS, new NBTTagLong(getStoredSpell().getCooldownInMilliseconds()));
+    }
 
-	private void setWandPython(ItemStack toPythonify) {
-		toPythonify.setTagInfo(SpellMetadataConstants.KEY_SPELL_PYTHON, new NBTTagString(getStoredSpell().getPythonScriptAsString()));
-	}
+    private void setWandPython(ItemStack toPythonify) {
+        toPythonify.setTagInfo(SpellMetadataConstants.KEY_SPELL_PYTHON, new NBTTagString(getStoredSpell().getPythonScriptAsString()));
+    }
 
-	private void setWandName(ItemStack toName) {
-		String displayName = getStoredSpell().getDisplayName();
-		if (displayName.equals(SpellMetadataConstants.NONE)) {
-			toName.setStackDisplayName("A Mysterious Magic Wand");
-		} else {
-			toName.setStackDisplayName("A Magic Wand of " + displayName);
-		}
-	}
+    private void setWandName(ItemStack toName) {
+        String displayName = getStoredSpell().getDisplayName();
+        if (displayName.equals(SpellMetadataConstants.NONE)) {
+            toName.setStackDisplayName("A Mysterious Magic Wand");
+        } else {
+            toName.setStackDisplayName("A Magic Wand of " + displayName);
+        }
+    }
 
-	private long timer() {
+    private long timer() {
         return System.currentTimeMillis() - lastCast;
     }
 
     public void attemptMagic(EntityPlayer spellcaster) {
         if (hasEnoughExperienceToUse(spellcaster) && timer() > storedSpell.getCooldownInMilliseconds()) {
             doMagic();
+            deductCastingCost(spellcaster);
             lastCast = System.currentTimeMillis();
         }
     }
 
-    protected boolean hasEnoughExperienceToUse(EntityPlayer player){
-		return player.experienceLevel >= this.storedSpell.getRequiredExperienceLevels();
-	}
+    protected void deductCastingCost(EntityPlayer spellcaster) {
+        if (this.storedSpell.getConsumedExperiencePoints() > 0) {
+            spellcaster.addExperience(-this.storedSpell.getConsumedExperiencePoints());
+        }
+    }
+
+    protected boolean hasEnoughExperienceToUse(EntityPlayer player) {
+        return player.experienceLevel >= this.storedSpell.getRequiredExperienceLevels() &&
+                player.experienceTotal >= this.storedSpell.getConsumedExperiencePoints();
+    }
 
     protected synchronized void castStoredSpell() {
         ThreadFactory.makeSpellThread(
@@ -84,13 +92,13 @@ public class BasicMagicItem {
         return this.storedSpell;
     }
 
-	public void readFromItem(ItemStack heldItemMainhand) {
-    	if(isMagicWand(heldItemMainhand)) {
-			MinecraftPythonScriptLoader.SINGLETON().writeToScript(heldItemMainhand.getTagCompound().getString(SpellMetadataConstants.KEY_SPELL_PYTHON));
-		}
-	}
+    public void readFromItem(ItemStack heldItemMainhand) {
+        if (isMagicWand(heldItemMainhand)) {
+            MinecraftPythonScriptLoader.SINGLETON().writeToScript(heldItemMainhand.getTagCompound().getString(SpellMetadataConstants.KEY_SPELL_PYTHON));
+        }
+    }
 
-	public void doMagic() {
-		castStoredSpell();
-	}
+    public void doMagic() {
+        castStoredSpell();
+    }
 }
