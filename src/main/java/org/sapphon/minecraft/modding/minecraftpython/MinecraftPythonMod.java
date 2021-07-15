306c338fc125d471555cba1a70be9db3d052687e
Sapphon <sapphon@gmail.com>
2021-07-15 03:04:24 -0400

    Release 0.7.2 [https://trello.com/c/8EpWCpRq]

--- src/main/java/org/sapphon/minecraft/modding/minecraftpython/MinecraftPythonMod.java
+++ src/main/java/org/sapphon/minecraft/modding/minecraftpython/MinecraftPythonMod.java
@@ -29,7 +29,7 @@ import org.sapphon.minecraft.modding.minecraftpython.proxy.DedicatedServerProxy;
 @Mod(modid = MinecraftPythonMod.MODID, version = MinecraftPythonMod.VERSION, name = MinecraftPythonMod.MODID)
 public class MinecraftPythonMod {
     public static final String MODID = "minecraftpython";
-    public static final String VERSION = "1.12.2-0.7.1";
+    public static final String VERSION = "1.12.2-0.7.2";
     public static final int SCRIPT_RUN_COOLDOWN = 1500;
     public static final Logger logger = LogManager.getLogger(MinecraftPythonMod.MODID);
 
