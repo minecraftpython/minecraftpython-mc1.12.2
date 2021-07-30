b5bd8dd77ec40360dfe4de64a5c2be90101143f7
Sapphon <sapphon@gmail.com>
2021-07-30 02:38:58 -0400

    WIP add 'gateway' recipe at init that keeps map of wand recipes dynamically [https://trello.com/c/e0FS0dAr]

---
+++ src/main/java/org/sapphon/minecraft/modding/minecraftpython/item/recipe/RecipePythonicWand.java
@@ -0,0 +1,67 @@
+package org.sapphon.minecraft.modding.minecraftpython.item.recipe;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.ShapedRecipes;
+import net.minecraft.util.NonNullList;
+import net.minecraft.world.World;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RecipePythonicWand extends ShapedRecipes {
+    private static RecipePythonicWand instance;
+    private Map<List<String>,ItemStack> ingredientsToWands;
+
+    public synchronized static RecipePythonicWand SINGLETON() {
+        if (instance == null) {
+            instance = new RecipePythonicWand("",3,3, NonNullList.create(), new ItemStack(Blocks.DIRT));
+        }
+        return instance;
+    }
+
+    private RecipePythonicWand(String group, int width, int height, NonNullList<Ingredient > ingredients, ItemStack result) {
+        super(group,width,height,ingredients,result);
+        ingredientsToWands = new LinkedHashMap<>();
+        this.setRegistryName("minecraftpython","recipePythonicWand");
+    }
+
+    public void registerRecipeForWand(ItemStack wand, List<String> itemNames){
+        this.ingredientsToWands.put(itemNames, wand);
+    }
+
+    @Override
+    public boolean matches(InventoryCrafting inv, World worldIn){
+        for (List<String> ingredientList : ingredientsToWands.keySet()) {
+            if(compareInvToItemList(inv, ingredientList)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public ItemStack getCraftingResult(InventoryCrafting inv)
+    {
+        for (List<String> ingredientList : ingredientsToWands.keySet()) {
+            if(compareInvToItemList(inv, ingredientList)){
+                return ingredientsToWands.get(ingredientList).copy();
+            }
+        }
+        return ItemStack.EMPTY;
+    }
+
+    private boolean compareInvToItemList(InventoryCrafting inv, List<String> itemNames) {
+        for(int i=0;i<itemNames.size();i++){
+            System.out.println("comparing " + inv.getStackInSlot(i).getItem().getUnlocalizedName() + " to " + "item."+itemNames.get(i));
+            if(inv.getStackInSlot(i).getItem().getUnlocalizedName().compareToIgnoreCase("item."+itemNames.get(i)) != 0){
+                return false;
+            }
+        }
+        System.out.println("Approving match");
+        return true;
+    }
+}
