f05cd9dbc1b8cbbec2f945dc914e3fd4bfd469d8
Sapphon <sapphon@gmail.com>
2021-09-10 01:19:07 -0400

    Remove word 'repair' from anvil keys [https://trello.com/c/8RGCaoQH]

--- src/main/java/org/sapphon/minecraft/modding/minecraftpython/spells/metadata/SpellMetadataConstants.java
+++ src/main/java/org/sapphon/minecraft/modding/minecraftpython/spells/metadata/SpellMetadataConstants.java
@@ -15,6 +15,6 @@ public class SpellMetadataConstants {
     public static final String KEY_SMELTING_INGREDIENT = "smelting_ingredient";
     public static final String KEY_MAXIMUM_USES = "uses";
     public static final String KEY_CRAFTING_INGREDIENTS = "crafting_ingredients";
-    public static final String KEY_ANVIL_ITEM = "anvil_repair_item";
-    public static final String KEY_ANVIL_COST = "anvil_repair_level_cost";
+    public static final String KEY_ANVIL_ITEM = "anvil_ingredient";
+    public static final String KEY_ANVIL_COST = "anvil_level_cost";
 }
